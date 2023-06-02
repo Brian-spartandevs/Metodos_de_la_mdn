b565a74d26d771a73bf4bad71c394f9d3fb58d1d
Brian Arrieta <brian@spartandevs.com>
2023-06-01 21:39:41 -0400

    [BrianArrieta] Method concant()

---
+++ metodos_de_array/concat().js
@@ -0,0 +1,9 @@
+/* el metodo concat() se usa para unir dos o mas arrays. este metodo no cambia los arrays existentes, si no que devuelve un nuevo array */
+
+let array1 = [2, 4, 5, 6];
+
+let array2 = [7, 8, 9, 10];
+
+let array3 = array1.concat(array2);
+
+console.log(array3);
