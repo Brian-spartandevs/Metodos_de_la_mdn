f0c14410e365d5cac0572e2b1e6b6cbb21e853e3
Brian Arrieta <brian@spartandevs.com>
2023-06-03 22:17:41 -0400

    [BrianArrieta] Method find()

---
+++ metodos_de_array/find().js
@@ -0,0 +1,5 @@
+/* el metodo find() devuelve el valor del primer elemento del array que cumple la funcion de prueba proporcionada */
+
+const array1 = [2, 4, 4, 6, 7, 7, 20];
+
+console.log(array1.find((element) => element > 10));
