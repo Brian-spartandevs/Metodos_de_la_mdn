2e0e97a14492589801e428fb21e8350ccdcd6bd4
Brian Arrieta <brian@spartandevs.com>
2023-06-06 14:46:01 -0400

    [BrianArrieta] Method Array.isArray()

---
+++ metodos_de_array/Array.isArray().js
@@ -0,0 +1,7 @@
+/* el metodo Array.isArray() determina si el valor pasado es un Array. */
+
+console.log(Array.isArray("foo"));
+
+console.log(Array.isArray([2, 5, 6]));
+
+console.log(Array.isArray({ 0: 1, 1: 2 }));
