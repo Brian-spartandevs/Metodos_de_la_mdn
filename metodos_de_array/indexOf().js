c79327c850f64475e671b3afcb419faa1c44dc73
Brian Arrieta <brian@spartandevs.com>
2023-06-06 14:30:26 -0400

    [BrianArrieta] Method indexOf()

---
+++ metodos_de_array/indexOf().js
@@ -0,0 +1,8 @@
+/* el metodo indexOf() retorna el primer indice donde se pueda encontrar un elemento dado en el array, o retorna -1 si el elemento no esta presente. */
+
+const array1 = [4, 8, 67,21,67];
+
+console.log(array1.indexOf(67));
+
+//el segundo parametro pasado es el indice de donde va a empezar a buscar
+console.log(array1.indexOf(67,3))
