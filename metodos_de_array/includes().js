d97047573bf377f8f0344df4613488c1b3426c4c
Brian Arrieta <brian@spartandevs.com>
2023-06-06 14:10:06 -0400

    [BrianArrieta] Method includes()

---
+++ metodos_de_array/includes().js
@@ -0,0 +1,11 @@
+/* el metodo includes() determina si una matriz incluye un determinado elemento, devuelve true o false segun corresponda. */
+
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+
+const pets = ["dog", "cat", "spider"];
+
+console.log(pets.includes("spider"));
+
+console.log(pets.includes("at"));
