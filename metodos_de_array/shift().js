bdc6b2f26344f73100e12df4b52c018e2ab77f9f
Brian Arrieta <brian@spartandevs.com>
2023-06-07 15:12:03 -0400

    [BrianArrieta] shift() Method

---
+++ metodos_de_array/shift().js
@@ -0,0 +1,7 @@
+/* el metodo shift() elimina el primer elemento del array y lo retorna. Este metodo modifica la longitud del array. */
+
+const array1 = [1, 2, 3, 4];
+
+console.log(array1.shift());
+
+console.log(array1);
