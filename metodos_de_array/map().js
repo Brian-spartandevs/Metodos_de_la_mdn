afb2df8f5fe750a74df4accbac981be7656cc591
Brian Arrieta <brian@spartandevs.com>
2023-06-07 11:50:29 -0400

    [BrianArrieta] Method map()

---
+++ metodos_de_array/map().js
@@ -0,0 +1,6 @@
+/* el metodo map() crea un nuevo array con los resutados de la llamada a la funcion indicada aplicados a cada uno de sus elementos. */
+
+const array1 = [12, 7, 24, 76, 21, 0];
+
+const result = array1.map((element) => element * 2);
+console.log(result);
