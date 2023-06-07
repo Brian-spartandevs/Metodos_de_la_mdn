f18ff6ea4fab51eb74c225388b7f62471d5ecc78
Brian Arrieta <brian@spartandevs.com>
2023-06-07 15:29:06 -0400

    [BrianArrieta] slice() Method

---
+++ metodos_de_array/slice().js
@@ -0,0 +1,7 @@
+/* el metodo slice devuelve una copia de una parte del array dentro de un nuevo array empezando del inicio hasta el fin (fin no incluido). El array original no se modificara. */
+
+const array1 = [13, 5, 6, 88, 9];
+
+const array2 = array1.slice(1);
+
+console.log(array2);
