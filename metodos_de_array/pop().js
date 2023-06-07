b7d8bf94b855cc50ec9d3ca11d3b59e49ad4b7c2
Brian Arrieta <brian@spartandevs.com>
2023-06-07 13:12:07 -0400

    [BrianArrieta] Method pop()

---
+++ metodos_de_array/pop().js
@@ -0,0 +1,7 @@
+/* el metodo pop() elimina el ultimo elemento de un array y lo devuelve. Este metodo cambia la longitud del array. */
+
+let array1 = [2, 4, 6, 7];
+
+console.log(array1.pop());
+
+console.log(array1);
