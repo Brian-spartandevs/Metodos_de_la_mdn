e45bf15191446605974a1877e8a8d5c9f90ead68
Brian Arrieta <brian@spartandevs.com>
2023-06-07 13:56:38 -0400

    [BrianArrieta] Method push()

---
+++ metodos_de_array/push().js
@@ -0,0 +1,7 @@
+/* el metodo push() anade uno o mas elementos al final de un array y devuelve la nueva longitud del array. */
+
+const pets = ["birds", "cats"];
+
+console.log(pets.push("dogs", "snake"));
+
+console.log(pets);
