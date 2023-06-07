4f4dd901826b3bc4f0b52e9a226d254f597f0bb6
Brian Arrieta <brian@spartandevs.com>
2023-06-07 15:02:06 -0400

    [BrianArrieta] Method reverse()

---
+++ metodos_de_array/reverse().js
@@ -0,0 +1,7 @@
+/* el metodo reverse() invierte el orden de los elementos de un array en su lugar. El primer elemento pasa a ser el ultimo y el ultimo pasa a ser le primero. La llamada a reverse() devuelve una referencia al array invertido. */
+
+const array1 = [2, 6, 9, 0];
+
+const reversed = array1.reverse();
+
+console.log(reversed);
