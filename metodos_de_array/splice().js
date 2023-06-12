b7475c3c2c78ff5e50e34a46f5cabcb146c59cdb
Brian Arrieta <brian@spartandevs.com>
2023-06-12 11:26:28 -0400

    [BrianArrieta] splice() Method

---
+++ metodos_de_array/splice().js
@@ -0,0 +1,7 @@
+/* el metodo splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos. */
+
+const months = ["Jan", "March", "April", "June"];
+
+months.splice(1, 0, "July");
+
+console.log(months);
