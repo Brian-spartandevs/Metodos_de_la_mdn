6d3799d414cb66f2df7dea0a44c62a1f254d2490
Brian Arrieta <brian@spartandevs.com>
2023-06-13 14:11:11 -0400

    [BrianArrieta] toSpliced() Method

---
+++ metodos_de_array/toSpliced().js
@@ -0,0 +1,8 @@
+/* el metodo toSpliced() de las instancias de Array. es la copia del metodo splice(). Devuelve una nueva matriz con algunos elementos eliminados y/o reemplazados de un indice determinado. */
+//nota: este es un nuevo metodo que saldra, a la fecha del subido este codigo no aparece nada.
+
+const array = [1, 2, 3, 4, 5];
+
+const newArray = array.toSpliced(1, 2, 10);
+
+console.log(newArray);
