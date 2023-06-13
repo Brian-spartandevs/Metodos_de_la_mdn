e8c229e7e8a8ddee8203226758abfa18ac274504
Brian Arrieta <brian@spartandevs.com>
2023-06-13 14:27:18 -0400

    [BrianArrieta] toString() Method

---
+++ metodos_de_array/toString().js
@@ -0,0 +1,6 @@
+/* el metodo toString() devuelve una cadena de caracteres representando el array especificado y sus 
+elementos. */
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array.toString());
