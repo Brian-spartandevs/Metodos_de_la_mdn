f5c10943b07939c1a686b4636d03662b2e6c9a53
Brian Arrieta <brian@spartandevs.com>
2023-06-13 12:19:16 -0400

    [BrianArrieta] toReversed() Method

---
+++ metodos_de_array/toReversed().js
@@ -0,0 +1,8 @@
+/* el metodo toReversed() de las instancias de array es la contraparte de copia del metodo reverse().  Devuelve una matriz con los elementos en orden invertido. */
+//nota: este es un nuevo metodo que saldra, a la fecha del subido este codigo no aparece nada.
+
+const array = [1, 2, 3, 4, 5];
+
+const newArrayReversed = array.toReversed();
+
+console.log(newArrayReversed);
