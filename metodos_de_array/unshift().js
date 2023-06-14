ce070fc048e4fecdde59706607c2c90583fbfbc0
Brian Arrieta <brian@spartandevs.com>
2023-06-14 19:42:22 -0400

    [BrianArrieta] unshift() Method

---
+++ metodos_de_array/unshift().js
@@ -0,0 +1,8 @@
+/* el metodo unshift() agrega uno o mas elementos al inicio del array, y devuelve la nueva longitud del
+array. */
+
+let array = [1, 2, 3, 4, 5];
+
+console.log(array.unshift("2"));
+
+console.log(array);
