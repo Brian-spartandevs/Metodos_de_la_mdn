060c74fcfad2405a1245934facddce60ed6dfe79
Brian Arrieta <brian@spartandevs.com>
2023-06-15 21:51:30 -0400

    [BrianArrieta] charCodeAt() Method

---
+++ metodos_de_strings/charCodeAt().js
@@ -0,0 +1,5 @@
+/* el metodo charCodeAt() devuelve un numero indicando un valor Unicode del caracter en el indice proporcionado. */
+
+console.log("ABC".charCodeAt(0));
+console.log("ABC".charCodeAt(1));
+console.log("ABC".charCodeAt(2));
