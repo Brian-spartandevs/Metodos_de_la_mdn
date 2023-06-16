6b7b3906f81cb2367ad9ced20ea0ca462ad46d13
Brian Arrieta <brian@spartandevs.com>
2023-06-16 10:03:41 -0400

    [BrianArrieta] endsWith() Method

---
+++ metodos_de_strings/endsWith().js
@@ -0,0 +1,8 @@
+/* el metodo endsWith() determina si una cadena de texto termina con los caracteres de una cadena indicada, devolviendo true o false segun corresponda. */
+
+const str1 = "Hello world!";
+
+console.log(str1.endsWith("world"));
+console.log(str1.endsWith("world!"));
+console.log(str1.endsWith("World!"));
+console.log(str1.endsWith("!"));
