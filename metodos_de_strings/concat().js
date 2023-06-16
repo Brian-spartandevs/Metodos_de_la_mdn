8570a46f55a60fb125ec9059b100bc4f67e0d4d1
Brian Arrieta <brian@spartandevs.com>
2023-06-16 09:58:14 -0400

    [BrianArrieta] concat() Method

---
+++ metodos_de_strings/concat().js
@@ -0,0 +1,9 @@
+/* el metodo concat() combina dos o mas cadenas de texto y devuelve una cadena de texto nueva. */
+
+const str1 = "Hi";
+
+const str2 = "World";
+
+const str3 = str1.concat(str2);
+
+console.log(str3);
