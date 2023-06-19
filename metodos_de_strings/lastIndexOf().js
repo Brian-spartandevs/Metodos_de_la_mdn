cd75cade91c6b4191ae86af6db89e9e65d4a067c
Brian Arrieta <brian@spartandevs.com>
2023-06-19 19:20:50 -0400

    [BrianArrieta] lastIndexOf() Method

---
+++ metodos_de_strings/lastIndexOf().js
@@ -0,0 +1,5 @@
+/* el metodo lastIndexOf() devuelve la posicion (indice) en la que se encuentra el valorBusqueda, dentro del objeto string que realiza la llamada, de la ultima ocurrencia del valor especificado; o -1 si no se halla. La busqueda se realiza empezando por el final de la cadena que realiza la llamada. */
+
+const str1 = "Saturday";
+
+console.log(str1.lastIndexOf("a"));
