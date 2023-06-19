daea2d73e262e7547cb0eb9455ce4b5b1ee489e7
Brian Arrieta <brian@spartandevs.com>
2023-06-19 14:01:18 -0400

    [BrianArrieta] indexOf() Method

---
+++ metodos_de_strings/indexOf().js
@@ -0,0 +1,7 @@
+/* el metodo indexOf() devuelve el indice, dentro del obejto string que realiza la llamada, de la primera ocurrencia del valor especificado. */
+
+const str1 = "Brian Esteban Arrieta Infante";
+
+console.log(str1.indexOf("Esteban"));
+console.log(str1.indexOf("Infante"));
+console.log(str1.indexOf("esteban"));
