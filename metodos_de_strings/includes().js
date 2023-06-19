9ab7b9a9da12b422d942e5f80b03f98e88d11ae5
Brian Arrieta <brian@spartandevs.com>
2023-06-19 13:30:37 -0400

    [BrianArrieta] includes() Method

---
+++ metodos_de_strings/includes().js
@@ -0,0 +1,9 @@
+/* el metodo includes() determina si una cadena de texto puede ser encontrada dentro de otra cadena de texto, devolviendo true o false segun corresponda. */
+
+const str1 = "who are you?";
+
+console.log(str1.includes("who"));
+console.log(str1.includes(" "));
+console.log(str1.includes("are"));
+console.log(str1.includes("you?"));
+console.log(str1.includes("  "));
