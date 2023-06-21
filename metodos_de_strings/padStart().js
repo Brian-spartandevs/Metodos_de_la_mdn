7e47eaca7e9c4aeabec99c1ed57d4bda13de86fd
Brian Arrieta <brian@spartandevs.com>
2023-06-21 19:05:49 -0400

    [BA] padStart() Method

---
+++ metodos_de_strings/padStart().js
@@ -0,0 +1,5 @@
+/* El método padStart() rellena la cadena actual con una cadena dada (repetida eventualmente) de modo que la cadena resultante alcance una longitud dada. El relleno es aplicado desde el inicio (izquierda) de la cadena actual. */
+
+"abc".padStart(10); // "       abc"
+"abc".padStart(10, "foo"); // "foofoofabc"
+"abc".padStart(6, "123465"); // "123abc"
