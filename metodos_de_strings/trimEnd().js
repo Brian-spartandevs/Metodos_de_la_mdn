5cdac1ce3a780cc90b030cd80b416d97d9ff474e
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:49:12 -0400

    [BA] trimEnd() Method

---
+++ metodos_de_strings/trimEnd().js
@@ -0,0 +1,9 @@
+/* El método trimEnd() quita el espacio en blanco del final de una cadena y devuelve una nueva cadena, sin modificar la cadena original. es un alias de este método. */
+
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trimEnd());
+// Expected output: "   Hello world!";
