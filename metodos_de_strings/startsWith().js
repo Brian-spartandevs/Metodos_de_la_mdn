7be63d68792169786cca6f7f5f60ca45b0a79fe8
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:27:02 -0400

    [BA] startsWith() Method

---
+++ metodos_de_strings/startsWith().js
@@ -0,0 +1,9 @@
+/* El método startsWith() determina si una cadena comienza con los caracteres de una cadena especificada, devolviendo o según corresponda.true o false */
+
+const str1 = 'Saturday night plans';
+
+console.log(str1.startsWith('Sat'));
+// Expected output: true
+
+console.log(str1.startsWith('Sat', 3));
+// Expected output: false
