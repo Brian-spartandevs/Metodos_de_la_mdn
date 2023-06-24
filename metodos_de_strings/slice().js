74ee9cf264de84dfc5d6b941202fde195c54d102
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:23:44 -0400

    [BA] slice() Method

---
+++ metodos_de_strings/slice().js
@@ -0,0 +1,5 @@
+/* El método slice() extrae una sección de una cadena y Lo devuelve como una nueva cadena, sin modificar la cadena original. */
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.slice(31));
