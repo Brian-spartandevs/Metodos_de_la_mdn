2fa949371d11a773291d1346f304db8966c9f4a2
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:44:02 -0400

    [BA] toString() Method

---
+++ metodos_de_strings/toUpperCase().js
@@ -0,0 +1,6 @@
+/* El método toUpperCase() devuelve el valor de cadena de llamada Convertido a mayúsculas (el valor se convertirá en una cadena si no es una). */
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toUpperCase());
+// Expected output: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
