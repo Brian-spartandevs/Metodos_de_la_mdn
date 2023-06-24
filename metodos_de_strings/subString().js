cf741a0bcdcc6f4dd4ee06c64bcaa9c46242c699
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:29:08 -0400

    [BA] subString() Method

---
+++ metodos_de_strings/subString().js
@@ -0,0 +1,5 @@
+/* El método substring() devuelve la parte del índice desde el inicio hasta y excluyendo el índice final, o hasta el final de la cadena si no se proporciona ningún índice final. string */
+
+const str = 'Mozilla';
+
+console.log(str.substring(1, 3));
