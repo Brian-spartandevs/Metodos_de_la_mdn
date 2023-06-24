20d6a8ae211b593946043d874fbbb3b8899910f5
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:43:01 -0400

    [BA] toString() Method

---
+++ metodos_de_strings/toString().js
@@ -0,0 +1,9 @@
+/* El método toString() devuelve una cadena que representa el valor de cadena especificado. */
+
+const stringObj = new String('foo');
+
+console.log(stringObj);
+// Expected output: String { "foo" }
+
+console.log(stringObj.toString());
+// Expected output: "foo"
