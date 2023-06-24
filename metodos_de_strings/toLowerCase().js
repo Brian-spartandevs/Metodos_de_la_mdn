37532422a05b8b56d337556759ceb153e624b0bf
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:39:47 -0400

    [BA] toLowerCase() Method

---
+++ metodos_de_strings/toLowerCase().js
@@ -0,0 +1,6 @@
+/* El método toLowerCase() devuelve el valor de cadena de llamada convertido a minúsculas. */
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toLowerCase());
+// Expected output: "the quick brown fox jumps over the lazy dog."
