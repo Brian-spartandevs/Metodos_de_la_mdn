0d2e4fe7ca88b44fe28add699e574434be6a1d69
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:09:05 -0400

    [BA] repeat() Method

---
+++ metodos_de_strings/repeat().js
@@ -0,0 +1,3 @@
+/* El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados. */
+
+console.log("str".repeat(6));
