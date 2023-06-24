4128aeee14ce5f2a2a6306b17bbb9424e243ade3
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:47:54 -0400

    [BA] trim() Method

---
+++ metodos_de_strings/trim().js
@@ -0,0 +1,9 @@
+/* El método trim() quita el espacio en blanco de ambos extremos de una cadena y devuelve una nueva cadena, sin modificar la cadena original. */
+
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// Expected output: "Hello world!";
