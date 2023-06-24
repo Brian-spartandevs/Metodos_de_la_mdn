412d42debd02891ae1107c77a7e64d40740d0f64
Brian Arrieta <brian@spartandevs.com>
2023-06-24 10:25:11 -0400

    [BA] split() Method

---
+++ metodos_de_strings/split().js
@@ -0,0 +1,6 @@
+/* El método split() toma un patrón y divide un String en una lista ordenada de subcadenas buscando el patrón, coloca estas subcadenas en una matriz y devuelve la matriz. */
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(words[3]);
