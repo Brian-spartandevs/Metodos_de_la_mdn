7b6333a514b41209aea1035dbbd32176e6873464
Brian Arrieta <brian@spartandevs.com>
2023-06-27 00:03:09 -0400

    [BA] create() Method

---
+++ metodos_de_objetos/create().js
@@ -0,0 +1,16 @@
+/* El método Object.create() crea un objeto nuevo, utilizando un objeto existente como el prototipo del nuevo objeto creado. */
+
+const person = {
+    isHuman: false,
+    printIntroduction: function() {
+      console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+    }
+  };
+  
+  const me = Object.create(person);
+  
+  me.name = 'Matthew'; // "name" is a property set on "me", but not on "person"
+  me.isHuman = true; // Inherited properties can be overwritten
+  
+  me.printIntroduction();
+  // Expected output: "My name is Matthew. Am I human? true"
