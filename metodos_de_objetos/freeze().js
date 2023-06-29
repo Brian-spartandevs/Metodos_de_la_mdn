423ff93a11159aa55e5286d9fbe0f2f872573d7f
Brian Arrieta <brian@spartandevs.com>
2023-06-29 19:01:08 -0400

    [BA] freeze() Method

---
+++ metodos_de_objetos/freeze().js
@@ -0,0 +1,13 @@
+/* El método Object.freeze() congela un objeto, es decir: impide que se le agreguen nuevas propiedades; impide que se puedan eliminar las propiedades ya existentes; impide que dichas propiedades, o su capacidad de enumeración, configuración, o escritura, puedan ser modificadas; impide también que se pueda modificar su prototipo. El método devuelve el objeto recibido. */
+
+const obj = {
+  prop: 42,
+};
+
+Object.freeze(obj);
+
+obj.prop = 33;
+// Throws an error in strict mode
+
+console.log(obj.prop);
+// Expected output: 42
