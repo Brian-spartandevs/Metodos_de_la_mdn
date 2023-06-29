7131af194c6854b7923908c52a2fdac954d115f8
Brian Arrieta <brian@spartandevs.com>
2023-06-29 19:04:07 -0400

    [BA] fromEntries() Method

---
+++ metodos_de_objetos/fromEntries().js
@@ -0,0 +1,11 @@
+/* El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto. */
+
+const entries = new Map([
+  ["foo", "bar"],
+  ["baz", 42],
+]);
+
+const obj = Object.fromEntries(entries);
+
+console.log(obj);
+// Expected output: Object { foo: "bar", baz: 42 }
