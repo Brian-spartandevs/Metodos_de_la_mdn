6d0bc722347aa4b6a29342c61f2fc562093138fb
Brian Arrieta <brian@spartandevs.com>
2023-07-02 22:09:06 -0400

    [BA] seal() Method

---
+++ metodos_de_objetos/seal().js
@@ -0,0 +1,18 @@
+/* El método Object.seal() sella un objeto, previniendo que puedan añadirse nuevas propiedades al mismo, y marcando todas las propiedades existentes como no-configurables. Los valores de las propiedades presentes permanecen pudiendo cambiarse en tanto en cuanto dichas propiedades sean de escritura. */
+
+const obj = {
+  prop1: "value1",
+  prop2: "value2",
+};
+
+console.log(Object.isSealed(obj)); // false
+
+Object.seal(obj);
+
+console.log(Object.isSealed(obj)); // true
+
+obj.prop1 = "new value";
+console.log(obj.prop1); // 'new value'
+
+delete obj.prop2;
+console.log(obj.prop2); // undefined
